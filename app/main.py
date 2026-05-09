bcbe4b9d00873f02f302962d1744a835f6941cab
Anderson <anderson.medeiros3011@gmail.com>
2026-05-08 22:36:54 -0300

    corrigir: ajustar anotações de estilo e tipo

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Person:
     people = {}
-    def __init__(self, name, age):
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
@@ -8,14 +9,18 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_instances = []
-    for person_dict in people_data:
-        person = Person(person_dict['name'], person_dict['age'])
+    for person_dict in people:
+        person = Person(
+            name=person_dict["name"],
+            age=person_dict["age"],
+        )
         person_instances.append(person)
 
-    for person_dict, person in zip(people_data, person_instances):
+    for person_dict, person in zip(people, person_instances):
         if "wife" in person_dict and person_dict["wife"] is not None:
-            person.wife = Person.people [person_dict["wife"]]
+            person.wife = Person.people[person_dict["wife"]]
 
         if "husband" in person_dict and person_dict["husband"] is not None:
-            person.husband = Person.people [person_dict["husband"]]
+            person.husband = Person.people[person_dict["husband"]]
+
     return person_instances
